f9075f03f8af385ddbe95a0bfb62d7ca01945f2d
Dennis Every <dennis@ansyra.com>
2023-07-20 14:31:47 -0400

    Add rubocop-rails, and rubocop-performance to rubocop.yml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    flash[:alert] = 'You are not authorized to perform this action.'
+    flash[:alert] = I18n.t('not_authorized')
     redirect_back(fallback_location: root_path)
   end
 end
